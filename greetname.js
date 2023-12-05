e015c500ddffe1c43f6cf013db9a53f115de628e
manish7511 <137864864+manish7511@users.noreply.github.com>
2023-12-05 18:52:41 +0530

    Add files via upload

---
+++ greetname.js
@@ -0,0 +1,10 @@
+// write a program to greet a person given their last and first name
+
+let firstname="Manish"
+let lastname="kumar"
+if(firstname && lastname){
+    console.log("Hello mr " + firstname + lastname)
+}
+else{
+    console.log("please provide first name or last name")
+}
