f46f51eb3071d5319c6e0df6c8f0a986fbadfce5
manish7511 <manishsingh7134@gmai.com>
2023-12-07 08:46:50 +0530

    biggest value

---
+++ biggest.js
@@ -0,0 +1,10 @@
+
+var arr = [3, 6, 2, 56, 32, 5, 9, 32];
+var largest = arr[0];
+
+for (var i = 0; i < arr.length; i++) {
+  if (arr[i] > largest ) {
+    largest = arr[i];
+  }
+}
+console.log(largest);
--- even.js
+++ even.js
@@ -1,5 +1,5 @@
-ages=[10,11,12,13,14,15,16,17,18,19,20];
-for(i=0;i<ages.length;i++){
+let ages=[10,11,12,13,14,15,16,17,18,19,20];
+for(let i=0;i<ages.length;i++){
     if(ages[i]%2==0){
         console.log(ages[i]);
     }
