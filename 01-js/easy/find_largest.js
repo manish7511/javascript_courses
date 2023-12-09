5d3d7620db758f16c33eade711cea7ce714d1d2b
manish7511 <manishsingh7134@gmai.com>
2023-12-09 18:23:06 +0530

    greaterno

---
+++ 01-js/easy/find_largest.js
@@ -0,0 +1,10 @@
+function findlargest(numbers){
+    let biggest=numbers[0];
+    for(let i=0;i<numbers.length;i++){
+        if(numbers[i]>biggest){
+            biggest=numbers[i];
+        }
+    }
+    return biggest
+}
+
