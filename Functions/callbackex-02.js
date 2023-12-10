591eeb11594c0e2b559347e26222b6b68db54d7e
manish7511 <manishsingh7134@gmai.com>
2023-12-10 17:22:43 +0530

    how callback work

---
+++ Functions/callbackex-02.js
@@ -0,0 +1,14 @@
+function square(n){
+    return n*n
+}
+function cube(n){
+    return n*n*n
+}
+function dosomething(a,b,fn){
+    const val1=fn(a);
+    const val2=fn(b);
+
+    return val1 + val2;
+}
+
+console.log(dosomething(2,2,square));
